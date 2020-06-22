5ad7d0a5622c55e5f98287ddf96e4ef25040f058
chriscauley <chris@lablackey.com>
2020-06-22 23:49:11 +0000

    adding hive images

---
+++ client/hive/pieces.js
@@ -0,0 +1,29 @@
+const custom = [
+  'assassin_bug',
+  'dragonfly',
+  'earthwork',
+  'earwig',
+  'firefly',
+  'mantis',
+  'shield_bug',
+  'tick',
+  'wasp',
+]
+
+const original = [
+  'ant',
+  'beetle',
+  'centipede',
+  'grasshopper',
+  'lady_bug',
+  'mosquito',
+  'pill_bug',
+  'queen',
+  'spider',
+]
+
+export default {
+  original,
+  custom,
+  all: original.concat(custom),
+}

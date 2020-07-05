4a04e1017b3bfaa5762d1db90e7e1563cf48154b
chriscauley <chris@lablackey.com>
2020-07-05 19:59:00 +0000

    moving unused resize code into separate file

--- client/game/Board/index.js
+++ client/game/Board/index.js
@@ -11,31 +11,6 @@ const board_cache = {}
 const PLAYER_COUNT = 2
 const noop = () => []
 
-export const resize = (board, dx, dy) => {
-  board.actions = [] // TODO issue #1
-  const old_geo = board.geo
-  board.W += dx
-  board.H += dy
-  board.geo = getGeo(board)
-  const new_stacks = {}
-  Object.entries(board.stacks).forEach(([index, stack]) => {
-    const xy = old_geo.index2xy(index)
-    new_stacks[board.geo.xy2index(xy)] = stack
-  })
-  board.stacks = new_stacks
-}
-
-const moveStacks = (board, dx, dy) => {
-  const new_stacks = {}
-  Object.entries(board.stacks).forEach(([index, stack]) => {
-    const xy = board.geo.index2xy(index)
-    xy[0] += dx
-    xy[1] += dy
-    new_stacks[board.geo.xy2index(xy)] = stack
-  })
-  board.stacks = new_stacks
-}
-
 const mantisCheck = (b, index) => {
   if (b.stacks[index].length > 1) {
     // mantis on hive can always move
@@ -61,10 +36,6 @@ const B = {
     b.actions = b.actions || []
     b.reverse = {}
     b.geo = getGeo(b)
-    let x_max = 0,
-      y_max = 0,
-      x_min = Infinity,
-      y_min = Infinity
     Object.entries(b.stacks).forEach(([index, stack]) => {
       if (!stack || stack.length === 0) {
         // prune unused stacks
@@ -74,25 +45,7 @@ const B = {
       stack.forEach((piece_id) => {
         b.reverse[piece_id] = parseInt(index)
       })
-      const [x, y] = b.geo.index2xy(index)
-      x_min = Math.min(x_min, x)
-      y_min = Math.min(y_min, y)
-      x_max = Math.max(x_max, x)
-      y_max = Math.max(y_max, y)
     })
-    if (x_min < 1) {
-      resize(b, 2, 0)
-      moveStacks(b, 2, 0)
-    } else if (x_max > b.W - 2) {
-      resize(b, 2, 0)
-    }
-
-    if (y_min < 1) {
-      resize(b, 0, 1)
-      moveStacks(b, 0, 1)
-    } else if (y_max > b.H - 2) {
-      resize(b, 0, 1)
-    }
 
     b.current_player = (b.turn % PLAYER_COUNT) + 1 // 1 on even, 2 on odd
     b.hash = B.current_hash = B.getHash(b)

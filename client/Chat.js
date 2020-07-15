6c4e875aa97564da11c1d06d25e0a01af68fb394
chriscauley <chris@lablackey.com>
2020-07-15 20:06:42 +0000

    Chat window now supports multiple rooms, also rearranging a bit

--- client/Chat.js
+++ client/Chat.js
@@ -2,16 +2,46 @@ import React from 'react'
 import css from '@unrest/css'
 import colyseus from './colyseus'
 
+const Minimized = ({ error, open }) => {
+  const icon = css.icon(error ? 'exclamation-triangle' : 'comments')
+  const button = css.button[error ? 'warning' : 'primary']('circle text-lg')
+  return (
+    <div className="fixed bottom-0 right-0 m-4">
+      <button
+        onClick={open}
+        className={button}
+        style={{ width: '2em', height: '2em' }}
+      >
+        <i className={icon} />
+      </button>
+    </div>
+  )
+}
+
+const Error = ({ close }) => (
+  <div
+    className="fixed bottom-0 right-0 border bg--bg"
+    style={{ width: 320 }}
+    onClick={close}
+  >
+    <div className="p-4">
+      <i className={css.icon('exclamation-triangle text-yellow-900 mr-2')} />
+      Unable to connect to server. Offline play only.
+    </div>
+  </div>
+)
+
 class Chat extends React.Component {
   state = {
     open: true,
+    current_room: 'general',
   }
 
   constructor(props) {
     super(props)
     this.listRef = React.createRef()
     this.textRef = React.createRef()
-    this.props.colyseus.joinRoom('general')
+    this.props.colyseus.joinOrCreateRoom({ channel: 'general' })
   }
 
   autoScroll = () => {
@@ -23,89 +53,82 @@ class Chat extends React.Component {
 
   open = () => this.setState({ open: true })
   close = () => this.setState({ open: false })
-  change = (e) => this.setState({ text: e.target.value })
   submit = (e) => {
     e.preventDefault()
     this.props.colyseus.send('general', 'chat', {
       text: this.textRef.current.textContent,
     })
-    this.setState({ text: '' })
+    this.textRef.current.textContent = ''
   }
 
   render() {
-    const { text, open, error } = this.state
-    const { user } = this.props.colyseus
+    const { open, error, current_room } = this.state
+    const { user, rooms } = this.props.colyseus
     if (!user) {
       return null
     }
     if (!open) {
-      const icon = css.icon(error ? 'exclamation-triangle' : 'comments')
-      const button = css.button[error ? 'warning' : 'primary']('circle text-lg')
-      return (
-        <div className="fixed bottom-0 right-0 m-4">
-          <button
-            onClick={this.open}
-            className={button}
-            style={{ width: '2em', height: '2em' }}
-          >
-            <i className={icon} />
-          </button>
-        </div>
-      )
+      return <Minimized open={this.open} error={error} />
     }
     if (error) {
-      return (
-        <div
-          className="fixed bottom-0 right-0 border bg--bg"
-          style={{ width: 320 }}
-          onClick={this.close}
-        >
-          <div className="p-4">
-            <i
-              className={css.icon('exclamation-triangle text-yellow-900 mr-2')}
-            />
-            Unable to connect to server. Offline play only.
-          </div>
-        </div>
-      )
+      return <Error close={this.close} />
     }
 
     this.autoScroll()
-    const { messages = [] } = this.props.colyseus
+    const room = this.props.colyseus[current_room] || {}
+    const { messages = [] } = room
+    const room_list = Object.keys(rooms).sort()
+    const _list = (n) =>
+      `cursor-pointer ${n === current_room ? 'font-bold' : ''}`
     return (
       <div
         className="fixed bottom-0 right-0 border bg--bg"
         style={{ width: 320 }}
       >
+        <ul className="border-b p-2 mb-2">
+          {room_list.map((name) => (
+            <li
+              key={name}
+              className={_list(name)}
+              onClick={() => this.setState({ current_room: name })}
+            >
+              {name === current_room && '* '}
+              {name}
+            </li>
+          ))}
+        </ul>
         <div className="bg-gray-200 py-1 px-2 text-right">
           <i
             className={css.icon('minus cursor-pointer')}
             onClick={this.close}
           />
         </div>
-        <div className="p-4" ref={this.ref}>
-          {messages.map(({ username, text }, i) => (
-            <p key={i}>
-              {username}: {text}
-            </p>
-          ))}
-        </div>
-
-        <form
-          id="form"
-          onSubmit={this.submit}
-          className="mb-0 border-t flex justify-between"
-        >
-          <span
-            className="block w-full"
-            ref={this.textRef}
-            contentEditable="true"
-          />
-          <button type="submit" className={css.icon('send')}></button>
-        </form>
+        <Messages
+          submit={this.submit}
+          textRef={this.textRef}
+          listRef={this.listRef}
+          messages={messages}
+        />
       </div>
     )
   }
 }
+
+const Messages = ({ messages, submit, textRef, listRef }) => (
+  <>
+    <div className="p-4" ref={listRef}>
+      {messages.map(({ username, text }, i) => (
+        <p key={i}>
+          {username}: {text}
+        </p>
+      ))}
+    </div>
+
+    <form onSubmit={submit} className="mb-0 border-t flex justify-between">
+      <span className="block w-full" ref={textRef} contentEditable="true" />
+      <button type="submit" className={css.icon('send')}></button>
+    </form>
+  </>
+)
 
 export default colyseus.connect(Chat)

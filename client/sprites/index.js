7f99c788b64a0ced68b0c9033a0d1d81c4734d74
chriscauley <chris@lablackey.com>
2020-08-13 13:26:31 +0000

    adding index to sprite debug views

--- client/sprites/index.js
+++ client/sprites/index.js
@@ -1,20 +1,40 @@
 import React from 'react'
-import { Switch, Route, useRouteMatch } from 'react-router-dom'
+import { Link, Switch, Route, useRouteMatch } from 'react-router-dom'
 
 import Hexes, { makeSprites } from './Hexes'
 import PieceGenerator from './PieceGenerator'
 import Tiles from './Tiles'
+
+function Index({ path }) {
+  const links = ['tiles', 'hexes', 'pieces']
+  return (
+    <div className="w-full flex">
+      <ul className="browser-default m-4">
+        {links.map((link) => (
+          <li key={link}>
+            <Link to={`${path}${link}/`}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default {
   makeSprites,
   Routes() {
     const { path } = useRouteMatch()
     return (
-      <Switch>
-        <Route path={`${path}tiles/`} component={Tiles} />
-        <Route path={`${path}hexes/`} component={Hexes} />
-        <Route path={`${path}pieces/`} component={PieceGenerator} />
-      </Switch>
+      <div className="overflow-auto">
+        <Switch>
+          <Route exact path={path}>
+            <Index path={path} />
+          </Route>
+          <Route path={`${path}tiles/`} component={Tiles} />
+          <Route path={`${path}hexes/`} component={Hexes} />
+          <Route path={`${path}pieces/`} component={PieceGenerator} />
+        </Switch>
+      </div>
     )
   },
 }

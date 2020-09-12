b38aaea181f63d2e6e8878b3a89dfbf27c36b629
chriscauley <chris@lablackey.com>
2020-09-12 13:20:20 +0000

    adding django channels (sample chat app from tutorial, will re-purpose now for board game)

--- server/settings/00-base.py
+++ server/settings/00-base.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'chat',
     'server',
 ]
 
@@ -48,7 +50,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'server.wsgi.application'
-
+ASGI_APPLICATION = 'server.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
--- server/urls.py
+++ server/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from server.views import room, message_room
+import chat.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/room/', room),
     path('api/room/message/', message_room),
+    path('chat/', chat.views.index, name='index'),
+    path('chat/<room_name>/', chat.views.room, name='room'),
     path('', include('unrest.urls')),
 ]

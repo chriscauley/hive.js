4f404057ca2107822f5eaaf1a2ad3e64e92b0a60
chriscauley <chris@lablackey.com>
2020-09-12 13:28:38 +0000

    moving routing out of chat and making asynchronous

--- server/routing.py
+++ server/routing.py
@@ -1,12 +1,15 @@
+from django.urls import re_path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+]
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })

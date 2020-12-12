2d80eeda662ea70b0bd9489ed0938261a2a2c94b
chriscauley <chris@lablackey.com>
2020-12-12 13:53:59 +0000

    cleaning up generation a bit (draw each hex seperately and then composite them)

--- client/sprites/Hexes.js
+++ client/sprites/Hexes.js
@@ -7,6 +7,31 @@ let style
 const SIZE = 100
 const LINE_WIDTH = (SIZE * Math.sqrt(3)) / 10 // 10% of height
 
+const fill_map = {
+  white2: '#FCFBF7', // eggshell lighten 10%
+  black2: '#333',
+  yellow: '#F1C40F',
+  green: '#4a4',
+  red: '#a44',
+  purple: '#84f',
+  blue: '#4af',
+  white: '#F6F3E7', // eggshell lighten 5%
+  black: '#000',
+  gray: 'gray',
+}
+
+const CLEAR = 'rgba(0, 0, 0, 0)'
+const a = 128
+const b = 96
+const bgs = {
+  empty: `rgba(${a},${a},${a},0.5)`,
+  empty_r: `rgba(${a},${b},${b},0.5)`,
+  empty_g: `rgba(${b},${a},${b},0.5)`,
+  empty_b: `rgba(${b},${b},${a},0.5)`,
+  player_1: fill_map.white2,
+  player_2: fill_map.black2,
+}
+
 const _hex = (ctx, color, x, y, r) => {
   ctx.beginPath()
   ctx.moveTo(x + r * Math.cos(0), y + r * Math.sin(0))
@@ -25,60 +50,17 @@ const _hex = (ctx, color, x, y, r) => {
   ctx.fill()
 }
 
-const fill_map = {
-  white: '#FCFBF7', // eggshell lighten 10%
-  black: '#333',
+const base_sprites = {
+  thick: 'white,black,red,blue,purple,green,yellow,gray',
+  thin: 'purple',
+  dashed: 'blue',
 }
 
-const border_map = {
-  yellow: '#F1C40F',
-  green: '#4a4',
-  red: '#a44',
-  purple: '#84f',
-  blue: '#4af',
-  white: '#F6F3E7', // eggshell lighten 5%
-}
-
-const makeHex = (canvas, fillStyle, strokeStyle) => {
-  if (!fillStyle && !strokeStyle) {
-    return
-  }
-  const outlineWidth = 1
-  let borderWidth = LINE_WIDTH
-  const ctx = canvas.getContext('2d')
-  const x = canvas.width / 2
-  const y = canvas.height / 2
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  let size = canvas.width / 2
-  if (!strokeStyle && fillStyle === 'white') {
-    _hex(ctx, 'black', x, y, size)
-    size -= outlineWidth
-  }
-  if (fill_map[fillStyle]) {
-    strokeStyle = strokeStyle || fillStyle
-    fillStyle = fill_map[fillStyle]
-  }
-  if (strokeStyle) {
-    const [color, style] = strokeStyle.split('-')
-    _hex(ctx, border_map[color] || color, x, y, size)
-    if (style === 'dashed') {
-      borderWidth *= 0.5
-      ctx.fillStyle = fillStyle.replace('0.5', '1')
-      // if (fillStyle.includes('rgba')) {
-      //   ctx.globalCompositeOperation = 'destination-out'
-      // }
-      for (let i = 0; i < 6; i++) {
-        const w = x / 2
-        ctx.translate(canvas.width / 2, canvas.height / 2)
-        ctx.rotate(Math.PI / 3)
-        ctx.translate(-canvas.width / 2, -canvas.height / 2)
-        ctx.fillRect(canvas.width / 2 - w / 2, 0, w, LINE_WIDTH, 0)
-      }
-      ctx.restore()
-    }
-    size -= borderWidth
-  }
-  _hex(ctx, fillStyle, x, y, size)
+const thin = 1 - 0.05 * Math.sqrt(3)
+const style_radius = {
+  thin,
+  thick: 1 - 0.1 * Math.sqrt(3),
+  dashed: thin,
 }
 
 const toStyle = (className, canvas, debug) => {
@@ -96,43 +78,77 @@ export const makeSprites = (debug) => {
   if (style) {
     return
   }
+  const img_cache = {}
+
+  const cacheImage = (id) => {
+    const c2 = document.createElement('canvas')
+    c2.width = canvas.width
+    c2.height = canvas.height
+    c2.getContext('2d').drawImage(canvas, 0, 0)
+    debug && append(c2)
+    img_cache[id] = c2
+  }
+
   const canvas = document.createElement('canvas')
   let style_text = ''
   debug && append(canvas)
   canvas.width = 2 * SIZE
   canvas.height = Math.sqrt(3) * SIZE
-  const a = 128
-  const b = 96
-  const bgs = {
-    empty: `rgba(${a},${a},${a},0.5)`,
-    empty_r: `rgba(${a},${b},${b},0.5)`,
-    empty_g: `rgba(${b},${a},${b},0.5)`,
-    empty_b: `rgba(${b},${b},${a},0.5)`,
-    player_1: 'white',
-    player_2: 'black',
+  const ctx = canvas.getContext('2d')
+  Object.entries(base_sprites).map(([style, colors]) => {
+    colors.split(',').forEach((color) => {
+      ctx.clearRect(0, 0, 2 * SIZE, 2 * SIZE)
+
+      _hex(ctx, fill_map[color], canvas.width / 2, canvas.height / 2, SIZE)
+      if (style_radius[style]) {
+        _hex(ctx, CLEAR, canvas.width / 2, canvas.height / 2, SIZE * style_radius[style])
+      }
+      if (style === 'dashed') {
+        for (let i = 0; i < 6; i++) {
+          const w = canvas.width / 4
+          ctx.translate(canvas.width / 2, canvas.height / 2)
+          ctx.rotate(Math.PI / 3)
+          ctx.translate(-canvas.width / 2, -canvas.height / 2)
+          ctx.clearRect(canvas.width / 2 - w / 2, -1, w, LINE_WIDTH, 0)
+        }
+        ctx.restore()
+      }
+      cacheImage(style + ',' + color)
+    })
+  })
+
+  const rims = {
+    player_1: img_cache['thick,white'],
+    player_2: img_cache['thick,black'],
   }
+
   const borders = [undefined, 'green', 'red', 'purple', 'gray', 'yellow', 'blue', 'blue-dashed']
   Object.entries(bgs).forEach(([bg_name, bg]) =>
     borders.forEach((border) => {
-      makeHex(canvas, bg, border, debug)
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      _hex(ctx, bg, canvas.width / 2, canvas.height / 2, SIZE)
       const cls = classNames('hex', 'hex-' + bg_name, border)
+      const rim = rims[bg_name]
+
+      if (rim) {
+        ctx.drawImage(rim, 0, 0)
+      }
+
+      if (border) {
+        const [border_color, style = 'thick'] = border.split('-')
+        ctx.drawImage(img_cache[style + ',' + border_color], 0, 0)
+      }
+
       style_text += toStyle(cls, canvas, debug)
 
-      if (['red', 'green'].includes(border) && bg_name === 'empty') {
-        const size = SIZE - LINE_WIDTH * 1.5
-        _hex(
-          canvas.getContext('2d'),
-          border_map['purple'],
-          canvas.width / 2,
-          canvas.height / 2,
-          size,
-        )
-        _hex(
-          canvas.getContext('2d'),
-          bg,
-          canvas.width / 2,
-          canvas.height / 2,
-          size - LINE_WIDTH / 2,
+      if (['green', 'yellow', 'red'].includes(border)) {
+        const _w = 0.15
+        ctx.drawImage(
+          img_cache['thin,purple'],
+          _w * canvas.width,
+          _w * canvas.height,
+          canvas.width * (1 - 2 * _w),
+          canvas.height * (1 - 2 * _w),
         )
         style_text += toStyle(cls + ' extra--dragonfly', canvas, debug)
       }
@@ -150,7 +166,7 @@ export default function Hexes() {
     <div>
       This is a page for viewing the sprites. These are generated every time the browser loads and
       are used as the background for <code>.hex</code> elements
-      <div className="sprite-box" />
+      <div className="sprite-box flex flex-wrap" />
     </div>
   )
 }

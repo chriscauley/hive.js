37a2f0feb1f4c7699e5a63d54c95c91f70905c62
chriscauley <chris@lablackey.com>
2021-04-19 00:18:31 +0000

    show time since last move

--- vue-client/src/components/TimeSince.vue
+++ vue-client/src/components/TimeSince.vue
@@ -10,18 +10,7 @@ export default {
     time: Number,
   },
   data() {
-    return { tick: 0, timeout: null }
-  },
-  computed: {
-    text() {
-      if (isNaN(this.time)) {
-        return ''
-      }
-      const seconds = Math.floor((new Date().valueOf() - this.time) / 1000)
-      const s = seconds % 60
-      const m = Math.floor(seconds / 60)
-      return `${fmt(m)}:${fmt(s)}`
-    },
+    return { text:'00:00', timeout: null }
   },
   mounted() {
     this.timeout = setTimeout(this.step, 1000)
@@ -34,6 +23,13 @@ export default {
       clearTimeout(this.timeout)
       this.tick++
       this.timeout = setTimeout(this.step, this.tick > 60 ? 60000 : 1000)
+      const seconds = Math.floor((new Date().valueOf() - this.time) / 1000)
+      if (isNaN(this.time)) {
+        return
+      }
+      const s = seconds % 60
+      const m = Math.floor(seconds / 60)
+      this.text = `${fmt(m)}:${fmt(s)}`
     },
   },
 }

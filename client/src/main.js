d349e4ce3bf71a57153199f6cf6631bbc8569aa0
chriscauley <chris@lablackey.com>
2021-08-03 13:19:36 +0000

    removing twitter login for now

--- client/src/main.js
+++ client/src/main.js
@@ -15,7 +15,7 @@ import '@/styles/base.scss'
 
 auth.configure({
   AUTH_START: '/',
-  oauth_providers: ['twitter', 'github'],
+  oauth_providers: ['github'],
   enabled: !process.env.VUE_APP_OFFLINE,
 })
 

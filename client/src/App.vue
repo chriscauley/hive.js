a039f2689f4682670aa2a61bee61fee54f18751b
chriscauley <379151+chriscauley@users.noreply.github.com>
2024-01-27 09:35:29 -0500

    renaming AppNav

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <Nav />
+  <app-nav />
   <router-view :class="`router-view ${debug ? 'debug' : ''}`" />
   <unrest-ui />
+  <piece-tutorial />
 </template>
 
 <script>
-import Nav from '@/components/Nav'
+import AppNav from '@/components/AppNav.vue'
+import PieceTutorial from '@/components/PieceTutorial.vue'
 
 export default {
   name: 'App',
-  components: { Nav },
+  components: { AppNav, PieceTutorial },
   computed: {
     debug() {
       return this.$store.config.state.debug
---
+++ client/src/components/PieceTutorial.vue
@@ -0,0 +1,14 @@
+<template>
+  <unrest-modal v-if="open">
+  </unrest-modal>
+</template>
+
+<script>
+export default {
+  computed: {
+    open() {
+      console.log(this.$route)
+    }
+  },
+}
+</script>
